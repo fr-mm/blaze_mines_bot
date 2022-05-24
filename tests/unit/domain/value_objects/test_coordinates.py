1efdd97e88f87b5847d559158dfb50e64079e33c
chico <chico.zimcolor@gmail.com>
2022-05-23 22:42:28 -0300

    created test for valid Coordinates

--- tests/unit/domain/value_objects/test_coordinates.py
+++ tests/unit/domain/value_objects/test_coordinates.py
@@ -5,6 +5,12 @@ from domain.value_objects import Coordinates
 
 
 class TestCoordinates(TestCase):
+    def test_init_WHEN_coordinates_are_positive_THEN_creates_instance(self) -> None:
+        x = 1
+        y = 1
+
+        Coordinates(x, y)
+
     def test_post_init_WHEN_x_is_negative_THEN_raises_invalid_coordinates_exception(self) -> None:
         x = -1
         y = 1
