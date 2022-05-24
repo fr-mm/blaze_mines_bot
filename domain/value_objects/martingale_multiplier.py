bbbcc83b2580eec61d95ab41e7acff64c2f722d0
chico <chico.zimcolor@gmail.com>
2022-05-24 18:34:00 -0300

    Implemented MartingaleMultiplier.to_string() and from_string()

--- domain/value_objects/martingale_multiplier.py
+++ domain/value_objects/martingale_multiplier.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 from dataclasses import dataclass
 
+from domain.enums import NumberFormatEnum
 from domain.exceptions import MartingaleMultiplierException
 
 
@@ -15,4 +17,18 @@ class MartingaleMultiplier:
         if self.value < self.__MIN_VALUE:
             raise MartingaleMultiplierException(
                 f'Must be at least {self.__MIN_VALUE}'
+            )
+
+    def to_string(self) -> str:
+        separator = NumberFormatEnum.DECIMAL_SEPARATOR.value
+        return str(self.value).replace('.', separator).replace(',', separator)
+
+    @staticmethod
+    def from_string(value: str) -> MartingaleMultiplier:
+        value = value.replace(',', '.')
+        try:
+            return MartingaleMultiplier(float(value))
+        except ValueError:
+            raise MartingaleMultiplierException(
+                f'Can not convert {value} to float'
             )
--- tests/unit/domain/value_objects/test_martingale_multiplier.py
+++ tests/unit/domain/value_objects/test_martingale_multiplier.py
@@ -15,3 +15,25 @@ class TestMartingaleMultiplier(TestCase):
 
         with self.assertRaises(MartingaleMultiplierException):
             MartingaleMultiplier(value)
+
+    def test_to_string_WHEN_called_THEN_returns_expected_string(self) -> None:
+        martingale_multiplier = MartingaleMultiplier(1.5)
+
+        result_string = martingale_multiplier.to_string()
+
+        expected_string = '1,5'
+        self.assertEqual(result_string, expected_string)
+
+    def test_from_string_WHEN_string_has_comma_THEN_returns_expected_martingale_multiplier(self) -> None:
+        input_string = '12,34'
+
+        result_martingale_multiplier = MartingaleMultiplier.from_string(input_string)
+
+        expected_martingale_multiplier = MartingaleMultiplier(12.34)
+        self.assertEqual(result_martingale_multiplier, expected_martingale_multiplier)
+
+    def test_from_string_WHEN_string_is_invalid_THEN_raises_martingale_multiplier_exception(self) -> None:
+        input_string = 'foo'
+
+        with self.assertRaises(MartingaleMultiplierException):
+            MartingaleMultiplier.from_string(input_string)
