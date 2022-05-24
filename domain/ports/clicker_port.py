d69c2304f9f269425eaae2eebdf7afe5848dee9a
chico <chico.zimcolor@gmail.com>
2022-05-24 10:44:11 -0300

    Updated ClickerPort

--- domain/ports/clicker_port.py
+++ domain/ports/clicker_port.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 
-from domain.value_objects import Coordinates
+from domain.value_objects import Coordinates, ScreenRegion
 
 
 class ClickerPort(ABC):
     @abstractmethod
-    def click_on(self, coordinates: Coordinates) -> None:
+    def click_on_coordinates(self, coordinates: Coordinates) -> None:
+        pass
+
+    @abstractmethod
+    def click_on_screen_region(self, screen_region: ScreenRegion) -> None:
         pass
