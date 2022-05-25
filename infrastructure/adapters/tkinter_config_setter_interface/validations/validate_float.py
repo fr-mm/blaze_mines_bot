5d36fb316221997e813de4961fb6d6bfe66c0c7a
chico <chico.zimcolor@gmail.com>
2022-05-25 20:32:51 -0300

    Fixed validate_float.py

--- infrastructure/adapters/tkinter_config_setter_interface/validations/validate_float.py
+++ infrastructure/adapters/tkinter_config_setter_interface/validations/validate_float.py
@@ -3,6 +3,8 @@ from domain.enums import NumberFormatEnum
 
 def validate_float(entry: str) -> bool:
     separator = NumberFormatEnum.DECIMAL_SEPARATOR.value
+    if not entry:
+        return True
     if not entry[-1].isdigit() and entry[-1] != separator:
         return False
     if entry.count(separator) > 1:
