4cbbf681a0944152ff6b944901c92c5f0e08364b
chico <chico.zimcolor@gmail.com>
2022-05-28 23:52:14 -0300

    Created MssScreenshooter

---
+++ infrastructure/adapters/opencv_mss_screen_reader/mss_screenshooter.py
@@ -0,0 +1,48 @@
+import os
+import time
+from typing import Dict
+
+import mss
+import mss.tools
+from mss.base import MSSBase
+
+from domain.value_objects import ImagePath, ScreenRegion
+
+
+class MssScreenshooter:
+    __SCREENSHOT_FILE_NAME = 'screenshot.jpg'
+    __mss: MSSBase
+    __path: str
+
+    def __init__(self) -> None:
+        self.__mss = mss.mss()
+        self.__path = MssScreenshooter.get_path()
+
+    def take_full_screenshot(self) -> ImagePath:
+        self.__mss.shot(output=self.__path)
+        return ImagePath(self.__SCREENSHOT_FILE_NAME)
+
+    def take_screenshot_from_region(self, screen_region: ScreenRegion) -> ImagePath:
+        mss_coordinates = self.__screen_region_to_mss_coordinates(screen_region)
+        screenshot = self.__mss.grab(mss_coordinates)
+        mss.tools.to_png(screenshot.rgb, size=screenshot.size, output=self.__path)
+        return ImagePath(self.__SCREENSHOT_FILE_NAME)
+
+    @staticmethod
+    def get_path() -> str:
+        static_directory = ImagePath.get_static_directory_path()
+        return os.path.join(static_directory, MssScreenshooter.__SCREENSHOT_FILE_NAME)
+
+    @staticmethod
+    def __screen_region_to_mss_coordinates(screen_region: ScreenRegion) -> Dict[str, int]:
+        return {
+            'top': screen_region.top_left.y,
+            'left': screen_region.top_left.x,
+            'width': screen_region.bottom_right.x - screen_region.top_left.x,
+            'height': screen_region.bottom_right.y - screen_region.top_left.y
+        }
+
+
+if __name__ == '__main__':
+    time.sleep(1)
+    MssScreenshooter().take_full_screenshot()
---
+++ tests/unit/infrastructure/adapters/opencv_mss_screen_reader/test_mss_screenshooter.py
@@ -0,0 +1,34 @@
+import os
+from unittest import TestCase
+
+from infrastructure.adapters.opencv_mss_screen_reader.mss_screenshooter import MssScreenshooter
+
+
+class TestMssScreenshooter(TestCase):
+    def setUp(self) -> None:
+        self.remove_screenshot()
+
+    def tearDown(self) -> None:
+        self.remove_screenshot()
+
+    @staticmethod
+    def remove_screenshot() -> None:
+        screenshot = MssScreenshooter.get_path()
+        if os.path.isfile(screenshot):
+            os.remove(screenshot)
+
+    def test_take_full_screenshot_WHEN_called_THEN_creates_image_file(self) -> None:
+        mss_screenshooter = MssScreenshooter()
+
+        mss_screenshooter.take_full_screenshot()
+
+        image_path = mss_screenshooter.get_path()
+        self.assertTrue(os.path.isfile(image_path))
+
+    def test_take_full_screenshot_WHEN_called_THEN_returns_image_path_with_expected_value(self) -> None:
+        mss_screenshooter = MssScreenshooter()
+
+        image_path = mss_screenshooter.take_full_screenshot()
+
+        expected_value = mss_screenshooter.get_path()
+        self.assertEqual(image_path.value, expected_value)
