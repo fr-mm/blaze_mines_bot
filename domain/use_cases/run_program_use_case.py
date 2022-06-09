ebc491349d462b4ead2a523d3b467304fff1ab66
chico <chico.zimcolor@gmail.com>
2022-06-09 04:25:07 -0300

    Refactored RunProgramUseCase

--- domain/use_cases/run_program_use_case.py
+++ domain/use_cases/run_program_use_case.py
@@ -16,7 +16,6 @@ class RunProgramUseCase(RunProgramUseCasePort):
     __profit: Profit
     __config: Config
     __current_martingale: int
-    __image_location_error: bool
     __wins: int
     __losses: int
     __turns: int
@@ -29,99 +28,159 @@ class RunProgramUseCase(RunProgramUseCasePort):
         self.__losses = 0
         self.__turns = 0
 
+    @property
+    def profit(self) -> Profit:
+        return self.__profit
+
+    @profit.setter
+    def profit(self, profit: Profit) -> None:
+        self.__profit = profit
+
+    @property
+    def current_martingale(self) -> int:
+        return self.__current_martingale
+
+    @current_martingale.setter
+    def current_martingale(self, current_martingale: int) -> None:
+        self.__current_martingale = current_martingale
+
+    @property
+    def current_bet(self) -> Money:
+        return self.__current_bet
+
+    @current_bet.setter
+    def current_bet(self, current_bet: Money) -> None:
+        self.__current_bet = current_bet
+
+    @property
+    def wins(self) -> int:
+        return self.__wins
+
+    @wins.setter
+    def wins(self, wins: int) -> None:
+        self.__wins = wins
+
+    @property
+    def losses(self) -> int:
+        return self.__losses
+
+    @losses.setter
+    def losses(self, losses: int) -> None:
+        self.__losses = losses
+
+    @property
+    def turns(self) -> int:
+        return self.__turns
+
+    @turns.setter
+    def turns(self, turns: int) -> None:
+        self.__turns = turns
+
+    @property
+    def config(self) -> Config:
+        return self.__config
+
     def execute(self, loop_forever: bool = True) -> None:
         self.__container.printer.print_line('Iniciando...')
-        self.__set_up()
+        self.set_up()
         if loop_forever:
             while True:
-                self.__main_loop()
+                self.main_loop()
         else:
-            self.__main_loop()
+            self.main_loop()
 
-    def __set_up(self) -> None:
-        self.__prompt_user_config()
-        self.__set_starting_bet()
+    def set_up(self) -> None:
+        self.prompt_user_config()
+        self.set_starting_bet()
 
-    def __main_loop(self) -> None:
+    def main_loop(self) -> None:
         self.__container.printer.print_line(f'Iniciando loop {self.__turns}')
-        self.__type_bet()
-        self.__click_on_start_game()
-        self.__click_on_square()
-        self.__manage_game_result()
+        self.type_bet()
+        self.click_on_start_game()
+        self.click_on_square()
+        self.manage_game_result()
 
-    def __prompt_user_config(self) -> None:
+    def prompt_user_config(self) -> None:
         self.__container.printer.print_line('Aguardando configurações')
         config = self.__container.config_setter_interface.prompt_user_config()
         if not config:
             quit()
         self.__config = config
 
-    def __click_on_start_game(self) -> None:
-        self.__click_on_image(ImageSet.START_GAME)
+    def click_on_start_game(self) -> None:
+        self.click_on_image(ImageSet.START_GAME)
 
-    def __click_on_square(self) -> None:
-        self.__click_on_image(ImageSet.SQUARE)
+    def click_on_square(self) -> None:
+        self.click_on_image(ImageSet.SQUARE)
 
-    def __click_on_money_sign(self) -> None:
-        self.__click_on_image(ImageSet.MONEY_SIGN)
+    def click_on_money_sign(self) -> None:
+        self.click_on_image(ImageSet.MONEY_SIGN)
 
-    def __click_on_withdraw_money(self) -> None:
-        self.__click_on_image(ImageSet.WITHDRAW_MONEY)
+    def click_on_withdraw_money(self) -> None:
+        self.click_on_image(ImageSet.WITHDRAW_MONEY)
 
-    def __click_on_image(self, image: Image) -> None:
+    def click_on_image(self, image: Image) -> None:
         self.__container.printer.print_line(f'Clicando em {image.name.value}')
         self.__container.click_on_image_service.execute(image)
-        self.__sleep()
+        self.sleep()
 
-    def __type_bet(self) -> None:
+    def type_bet(self) -> None:
         self.__container.printer.print_line('Digitando aposta')
         self.__container.click_on_image_service.execute(ImageSet.MONEY_SIGN)
         self.__container.typer.type_money(self.__current_bet)
-        self.__sleep()
+        self.sleep()
 
-    def __set_starting_bet(self) -> None:
-        self.__set_bet(money=self.__config.starting_bet)
+    def set_starting_bet(self) -> None:
+        self.set_bet(money=self.__config.starting_bet)
 
-    def __set_bet(self, money: Money) -> None:
-        self.__container.printer.print_line(f'Configurando aposta: R${self.__current_bet}')
+    def set_bet(self, money: Money) -> None:
+        self.__container.printer.print_line(f'Configurando aposta: R${money.to_string()}')
         self.__current_bet = money
 
-    def __manage_game_result(self) -> None:
+    def manage_game_result(self) -> None:
         self.__container.printer.print_line('Analizando resultado')
         game_result = self.__container.get_game_result_service.execute()
         if game_result == GameResultEnum.WIN:
-            self.__manage_win()
+            self.manage_win()
         elif game_result == GameResultEnum.LOSS:
-            self.__manage_loss()
+            self.manage_loss()
         else:
             raise UnexpectedGameResultException()
 
-    def __manage_win(self) -> None:
+    def manage_win(self) -> None:
         self.__container.printer.print_line('Processando vitória')
-        self.__click_on_withdraw_money()
+        self.click_on_withdraw_money()
         self.__profit.sum(self.__current_bet)
-        self.__set_starting_bet()
+        self.set_starting_bet()
         self.__wins += 1
         self.__turns += 1
 
-    def __manage_loss(self) -> None:
+    def manage_loss(self) -> None:
         self.__container.printer.print_line('Processando derrota')
         if self.__current_martingale <= self.__config.max_martingales.value:
-            self.__current_martingale += 1
-            self.__container.printer.print_line(f'Aplicando {self.__current_martingale}° Martingale')
-            self.__multiply_bet_by_martingale()
-            self.__turns += 1
-            self.__print_current_profit()
+            self.apply_martingale_to_loss_result()
         else:
-            self.__container.printer.print_line(f'Máximo de {self.__config.max_martingales.value} excedido. Reconfigurando aposta inicial')
-            self.__current_martingale = 0
-            self.__profit.subtract(self.__current_bet)
-            self.__print_current_profit()
-            self.__set_starting_bet()
-            self.__losses += 1
-            self.__turns += 1
+            self.manage_exceeded_martingale_loss()
 
-    def __print_current_profit(self) -> None:
+    def apply_martingale_to_loss_result(self) -> None:
+        self.__current_martingale += 1
+        self.__container.printer.print_line(f'Aplicando {self.__current_martingale}° Martingale')
+        self.__profit = self.__profit.subtract(self.__current_bet)
+        self.multiply_bet_by_martingale()
+        self.__turns += 1
+        self.print_current_profit()
+
+    def manage_exceeded_martingale_loss(self) -> None:
+        self.__container.printer.print_line(
+            f'Máximo de {self.__config.max_martingales.value} excedido. Reconfigurando aposta inicial')
+        self.__current_martingale = 0
+        self.__profit = self.__profit.subtract(self.__current_bet)
+        self.print_current_profit()
+        self.set_starting_bet()
+        self.__losses += 1
+        self.__turns += 1
+
+    def print_current_profit(self) -> None:
         if self.__profit.value < 0:
             absolute_profit = self.__profit.to_string().replace('-', '')
             formatted_profit = f'-R${absolute_profit}'
@@ -129,9 +188,9 @@ class RunProgramUseCase(RunProgramUseCasePort):
             formatted_profit = f'R#{self.__profit.to_string()}'
         self.__container.printer.print_line(f'Lucro atual: {formatted_profit}')
 
-    def __multiply_bet_by_martingale(self) -> None:
+    def multiply_bet_by_martingale(self) -> None:
         bet = self.__current_bet.multiply(self.__config.martingale_multiplier)
-        self.__set_bet(bet)
+        self.set_bet(bet)
 
-    def __sleep(self) -> None:
+    def sleep(self) -> None:
         time.sleep(self.__config.seconds_between_actions.value)
--- tests/unit/domain/use_cases/test_run_program_use_case.py
+++ tests/unit/domain/use_cases/test_run_program_use_case.py
@@ -1,19 +1,25 @@
 import time
 from unittest import TestCase
 
-from mockito import mock, unstub, when
+from mockito import mock, unstub, when, verify
 
 from domain.aggregates import Config
 from domain.containers import RunProgramUseCaseContainer
+from domain.entities import Image
+from domain.enums import GameResultEnum
 from domain.services import PrinterService
 from domain.ports import ClickerPort, TyperPort, KeyboardListenerPort, ScreenReaderPort, ConfigSetterInterfacePort, \
     LocateImageInScreenUseCasePort, GetGameResultUseCasePort
+from domain.sets.image_set import ImageSet
 from domain.use_cases import RunProgramUseCase, ClickOnImageUseCase
-from domain.value_objects import ScreenRegion, ImagePath
+from domain.value_objects import ScreenRegion, ImagePath, Seconds, Money, Profit, MartingaleMultiplier
 
 
 class TestRunProgramUseCase(TestCase):
     def setUp(self) -> None:
+        self.screen_region_mock = mock(ScreenRegion)
+        self.image_path_mock = mock(ImagePath)
+        self.config = Config()
         self.clicker_mock = mock(ClickerPort)
         self.typer_mock = mock(TyperPort)
         self.keyboard_listener_mock = mock(KeyboardListenerPort)
@@ -23,6 +29,11 @@ class TestRunProgramUseCase(TestCase):
         self.printer_mock = mock(PrinterService)
         self.click_on_image_service = mock(ClickOnImageUseCase)
         self.get_game_result_service = mock(GetGameResultUseCasePort)
+
+        when(self.printer_mock).print_line(...)
+        when(self.printer_mock).print_open_line(...)
+        when(self.printer_mock).close_line()
+        when(RunProgramUseCase).sleep()
 
         self.container = RunProgramUseCaseContainer(
             clicker=self.clicker_mock,
@@ -35,24 +46,249 @@ class TestRunProgramUseCase(TestCase):
             click_on_image_service=self.click_on_image_service,
             get_game_result_service=self.get_game_result_service
         )
-
-        self.screen_region_mock = mock(ScreenRegion)
-        self.image_path_mock = mock(ImagePath)
-        self.config = Config()
-
-        when(self.clicker_mock).click_on_screen_region(...)
-        when(self.typer_mock).type_money(...)
-        when(self.keyboard_listener_mock).set_callback_to_esc_key(...)
-        when(self.screen_reader_mock).get_image_location(...).thenReturn(self.screen_region_mock)
-        when(self.screen_reader_mock).image_is_in_region(..., ...).thenReturn(True)
-        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
-        when(self.locate_image_in_screen_service_mock).execute(...)
-        when(self.printer_mock).print_line(...)
-        when(self.click_on_image_service).execute(...)
-        when(time).sleep(...)
+        self.events = []
 
     def tearDown(self) -> None:
         unstub()
+        self.reset_images_locations()
+        self.events = []
+
+    @staticmethod
+    def reset_images_locations() -> None:
+        ImageSet.SQUARE.location = None
+        ImageSet.DIAMOND.location = None
+        ImageSet.BOMB.location = None
+        ImageSet.START_GAME.location = None
+        ImageSet.MONEY_SIGN.location = None
+        ImageSet.WITHDRAW_MONEY.location = None
+
+    def record_event(self, event: str) -> None:
+        self.events.append(event)
 
     def test_init_WHEN_called_THEN_returns_instance(self) -> None:
-        RunProgramUseCase(self.container)
+        RunProgramUseCase(container=self.container)
+
+    def test_execute_WHEN_called_THEN_calls_set_up_before_calling_main_loop(self) -> None:
+        set_up_event = 'SET UP'
+        main_loop_event = 'MAIN LOOP'
+        when(RunProgramUseCase).set_up().thenReturn(self.record_event(set_up_event))
+        when(RunProgramUseCase).main_loop().thenReturn(self.record_event(main_loop_event))
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.execute(loop_forever=False)
+
+        expected_event_order = [set_up_event, main_loop_event]
+        self.assertEqual(self.events, expected_event_order)
+
+    def test_set_up_WHEN_called_THEN_calls_prompt_user_config_and_then_set_starting_bet(self) -> None:
+        prompt_user_config_event = 'PROMPT USER CONFIG'
+        set_starting_bet_event = 'SET STARTING BET'
+        when(RunProgramUseCase).prompt_user_config().thenReturn(self.record_event(prompt_user_config_event))
+        when(RunProgramUseCase).set_starting_bet().thenReturn(self.record_event(set_starting_bet_event))
+        when(RunProgramUseCase).main_loop()
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.execute(loop_forever=False)
+
+        expected_event_order = [prompt_user_config_event, set_starting_bet_event]
+        self.assertEqual(self.events, expected_event_order)
+
+    def test_main_loop_WHEN_called_THEN_calls_expected_methods_in_order(self) -> None:
+        type_bet_event = 'TYPE_BET'
+        click_on_start_game_event = 'CLICK ON START GAME'
+        click_on_square_event = 'CLICK ON SQUARE EVENT'
+        manage_game_result_event = 'MANAGE GAME RESULT'
+        when(RunProgramUseCase).type_bet().thenReturn(self.record_event(type_bet_event))
+        when(RunProgramUseCase).click_on_start_game().thenReturn(self.record_event(click_on_start_game_event))
+        when(RunProgramUseCase).click_on_square().thenReturn(self.record_event(click_on_square_event))
+        when(RunProgramUseCase).manage_game_result().thenReturn(self.record_event(manage_game_result_event))
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.execute(loop_forever=False)
+
+        expected_event_order = [
+            type_bet_event,
+            click_on_start_game_event,
+            click_on_square_event,
+            manage_game_result_event
+        ]
+        self.assertEqual(self.events, expected_event_order)
+
+    def test_prompt_user_config_WHEN_called_THEN_assigns_config_setter_interface_return_to_config(self) -> None:
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_interface = RunProgramUseCase(container=self.container)
+
+        run_program_interface.prompt_user_config()
+
+        self.assertEqual(run_program_interface.config, self.config)
+
+    def test_click_on_image_WHEN_image_given_THEN_calls_click_on_image_service_execute_with_given_image(self) -> None:
+        image = ImageSet.BOMB
+        when(self.click_on_image_service).execute(image)
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.click_on_image(image)
+
+        verify(self.click_on_image_service).execute(image)
+
+    def test_type_bet_WHEN_current_bet_is_set_THEN_calls_typer_type_money_with_current_bet(self) -> None:
+        current_bet = Money(100)
+        when(self.click_on_image_service).execute(ImageSet.MONEY_SIGN)
+        when(self.typer_mock).type_money(current_bet)
+        run_program_interface = RunProgramUseCase(container=self.container)
+        run_program_interface.set_bet(current_bet)
+
+        run_program_interface.type_bet()
+
+        verify(self.typer_mock).type_money(current_bet)
+
+    def test_manage_game_result_WHEN_result_is_win_THEN_calls_manage_win(self) -> None:
+        when(self.get_game_result_service).execute().thenReturn(GameResultEnum.WIN)
+        when(RunProgramUseCase).manage_win()
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.manage_game_result()
+
+        verify(RunProgramUseCase).manage_win()
+
+    def test_manage_game_result_WHEN_result_is_loss_THEN_calls_manage_loss(self) -> None:
+        when(self.get_game_result_service).execute().thenReturn(GameResultEnum.LOSS)
+        when(RunProgramUseCase).manage_loss()
+        run_program_service = RunProgramUseCase(container=self.container)
+
+        run_program_service.manage_game_result()
+
+        verify(RunProgramUseCase).manage_loss()
+
+    def test_apply_martingale_to_loss_result_WHEN_called_THEN_increments_current_martingale(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.current_martingale = 3
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.apply_martingale_to_loss_result()
+
+        expected_current_martingale = 4
+        self.assertEqual(run_program_service.current_martingale, expected_current_martingale)
+
+    def test_apply_martingale_to_loss_result_WHEN_called_THEN_calls_multiply_bet_by_martingale(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.apply_martingale_to_loss_result()
+
+        verify(RunProgramUseCase).multiply_bet_by_martingale()
+
+    def test_apply_martingale_to_loss_result_WHEN_called_THEN_increments_turns(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.turns = 3
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.apply_martingale_to_loss_result()
+
+        expected_turns = 4
+        self.assertEqual(run_program_service.turns, expected_turns)
+
+    def test_apply_martingale_to_loss_result_WHEN_has_profit_THEN_subtracts_bet_from_profit(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.profit = Profit(300)
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.apply_martingale_to_loss_result()
+
+        expected_profit = Profit(200)
+        self.assertEqual(run_program_service.profit, expected_profit)
+
+    def test_manage_exceeded_martingale_loss_WHEN_called_THEN_resets_current_martingale(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        when(RunProgramUseCase).set_starting_bet()
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        run_program_service.current_bet = Money(100)
+        run_program_service.current_martingale = 8
+
+        run_program_service.manage_exceeded_martingale_loss()
+
+        expected_current_martingale = 0
+        self.assertEqual(run_program_service.current_martingale, expected_current_martingale)
+
+    def test_manage_exceeded_martingale_loss_WHEN_has_profit_THEN_subtracts_bet_from_profit(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        when(RunProgramUseCase).set_starting_bet()
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        run_program_service.profit = Profit(300)
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.manage_exceeded_martingale_loss()
+
+        expected_profit = Profit(200)
+        self.assertEqual(run_program_service.profit, expected_profit)
+
+    def test_manage_exceeded_martingale_loss_WHEN_called_THEN_calls_set_starting_bet(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        when(RunProgramUseCase).set_starting_bet()
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        run_program_service.current_bet = Money(100)
+
+        run_program_service.manage_exceeded_martingale_loss()
+
+        verify(RunProgramUseCase).set_starting_bet()
+
+    def test_manage_exceeded_martingale_loss_WHEN_called_THEN_increments_losses(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        when(RunProgramUseCase).set_starting_bet()
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        run_program_service.current_bet = Money(100)
+        run_program_service.losses = 8
+
+        run_program_service.manage_exceeded_martingale_loss()
+
+        expected_losses = 9
+        self.assertEqual(run_program_service.losses, expected_losses)
+
+    def test_manage_exceeded_martingale_loss_WHEN_called_THEN_increments_turns(self) -> None:
+        when(RunProgramUseCase).multiply_bet_by_martingale()
+        when(RunProgramUseCase).print_current_profit()
+        when(RunProgramUseCase).set_starting_bet()
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(self.config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        run_program_service.current_bet = Money(100)
+        run_program_service.turns = 8
+
+        run_program_service.manage_exceeded_martingale_loss()
+
+        expected_turns = 9
+        self.assertEqual(run_program_service.turns, expected_turns)
+
+    def test_multiply_bet_by_martingale_WHEN_bet_is_set_THEN_multiplies_it_by_martingale_multiplier(self) -> None:
+        config = Config(martingale_multiplier=MartingaleMultiplier(3))
+        when(self.config_setter_interface_mock).prompt_user_config().thenReturn(config)
+        run_program_service = RunProgramUseCase(container=self.container)
+        run_program_service.prompt_user_config()
+        previous_bet = Money(100)
+        run_program_service.current_bet = previous_bet
+
+        run_program_service.multiply_bet_by_martingale()
+
+        expected_bet = Money(300)
+        self.assertEqual(run_program_service.current_bet, expected_bet)
